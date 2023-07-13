742f2df6b5d483567eb1bfdbd651a9318d5f1728
Aan Patel <patelaan13@gmail.com>
2023-07-12 22:47:39 -0400

    Update recaptcha provider

--- src/shared/Constants.ts
+++ src/shared/Constants.ts
@@ -1,2 +1,2 @@
 export const DEV = process.env.NODE_ENV === 'development';
-export const RECAPTCHA_PROVIDER = '6LcHB9UeAAAAANIL43h2hrmI8E4GRGhN-qiz_ljh';
+export const RECAPTCHA_PROVIDER = '6LfOxqQfAAAAAAK3usUKC8bM-BOqRNzEz4W5DOjg';
