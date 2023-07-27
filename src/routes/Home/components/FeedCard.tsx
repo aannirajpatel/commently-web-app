1cf3be6c100914791058f6f9f708456bec682fbf
Aan Patel <patelaan13@gmail.com>
2023-07-27 04:03:39 -0400

    fix: dummy feed header click should navigate to comments page for the given URL

--- src/routes/Home/components/FeedCard.tsx
+++ src/routes/Home/components/FeedCard.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 import { IPage } from '../../../DTO/Page/IPage';
+import { useNavigate } from "react-router-dom";
+import { getCommentsPagePath } from "../../../shared/Pages";
 
 const FeedCard = (props:IPage) => {
+    const navigateTo = useNavigate();
   return (
     <Card fluid>
       {props.img && <Image src={props.img} alt={props.title} wrapped ui={false} />}
       <Card.Content>
-        <Card.Header>{props.title}</Card.Header>
+        <Card.Header onClick={()=>{navigateTo(getCommentsPagePath(props.canonicalUrl))}} style={{cursor:'pointer'}}>{props.title}</Card.Header>
         <Card.Meta>
           <a href={props.canonicalUrl}>{props.canonicalUrl}</a>
         </Card.Meta>
